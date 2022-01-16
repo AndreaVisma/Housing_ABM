1bccf22ff4bacec5484b58fd4a63ca0c0f31f7a5
AndreaVisma <62595666+AndreaVisma@users.noreply.github.com>
2022-01-17 00:27:18 +0100

    more graphs

--- output_analysis.py
+++ output_analysis.py
@@ -446,7 +446,7 @@ sns.lineplot(ax = ax[1], data = df_avgs_SH, x = '[step]', y = 'segregation_upper
                         palette = 'rocket', label ='upper class')
 
 ax[0].set_title('No social housing')
-ax[0].set_ylabel('Average number neighbors of the same class')
+ax[0].set_ylabel('Average number services and green areas as neighbours')
 ax[0].set_xlabel('Simulation time')
 ax[1].set_xlabel('Simulation time')
 ax[1].set_title('With social housing')
@@ -476,7 +476,7 @@ sns.lineplot(ax = ax[1], data = df_avgs_SH, x = '[step]', y = 'upper-avg-distanc
                         palette = 'rocket', label ='upper class')
 
 ax[0].set_title('No social housing')
-ax[0].set_ylabel('Average number neighbors of the same class')
+ax[0].set_ylabel('Average distance from services')
 ax[0].set_xlabel('Simulation time')
 ax[1].set_xlabel('Simulation time')
 ax[1].set_title('With social housing')
@@ -504,7 +504,7 @@ sns.lineplot(ax = ax[1], data = df_avgs_SH, x = '[step]', y = 'upper-avg-distanc
                         palette = 'rocket', label ='upper class')
 
 ax[0].set_title('No social housing')
-ax[0].set_ylabel('Average number neighbors of the same class')
+ax[0].set_ylabel('Average distance from green areas')
 ax[0].set_xlabel('Simulation time')
 ax[1].set_xlabel('Simulation time')
 ax[1].set_title('With social housing')
@@ -515,9 +515,108 @@ fig.get_figure().savefig(output_fol + "normative_segregation_distance_green.pdf"
 plt.show()
 plt.close()
 
+#%% Distribution in dsitricts
 
+def horizontal_barplot(df_here):
+    """
+    Parameters
+    ----------
+    df : pandas dataframe from which to extrapolate the data
+    """
+    
+    classes = ['working class', 'middle class', 'upper class']
+    
+    hh_distr_1 = np.mean(df_here['working-district-1'] + df_here['middle-district-1']
+                                    + df_here['upper-district-1'])
+    hh_distr_2 = np.mean(df_here['working-district-2'] + df_here['middle-district-2']
+                                    + df_here['upper-district-2'])
+    hh_distr_3 = np.mean(df_here['working-district-3'] + df_here['middle-district-3']
+                                    + df_here['upper-district-3'])
+    hh_distr_4 = np.mean(df_here['working-district-4'] + df_here['middle-district-4']
+                                    + df_here['upper-district-4'])
+    hh_centre = np.mean(df_here['working-centre'] + df_here['middle-centre']
+                                    + df_here['upper-centre'])
+    hh_periphery = np.mean(df_here['working-periphery'] + df_here['middle-periphery']
+                                    + df_here['upper-periphery'])
+    
+    distribution = {
+        'District 1': np.round([np.mean(df_here['working-district-1']) / hh_distr_1, np.mean(df_here['middle-district-1']) / hh_distr_1,
+             np.mean(df_here['upper-district-1']) / hh_distr_1], 3),
+        'District 2': np.round([ np.mean(df_here['working-district-2']) / hh_distr_2,  np.mean(df_here['middle-district-2']) / hh_distr_2,
+              np.mean(df_here['upper-district-2']) / hh_distr_2], 3),
+        'District 3': np.round([ np.mean(df_here['working-district-3']) / hh_distr_3,  np.mean(df_here['middle-district-3']) / hh_distr_3,
+             np.mean(df_here['upper-district-3']) / hh_distr_3], 3),
+        'District 4': np.round([np.mean(df_here['working-district-4']) / hh_distr_4,  np.mean(df_here['middle-district-4']) / hh_distr_4,
+              np.mean(df_here['upper-district-4']) / hh_distr_4], 3),
+        'Inner City': np.round([ np.mean(df_here['working-centre']) / hh_centre,  np.mean(df_here['middle-centre']) / hh_centre,
+              np.mean(df_here['upper-centre']) / hh_centre], 3),
+        'Periphery': np.round([ np.mean(df_here['working-periphery']) / hh_periphery,  np.mean(df_here['middle-periphery']) / hh_periphery,
+              np.mean(df_here['upper-periphery']) / hh_periphery], 3)
+        }
+    
+    labels = list(distribution.keys())
+    data = np.array(list(distribution.values()))
+    data_cum = data.cumsum(axis=1)
+    category_colors = sns.color_palette("hls", 3)
+    
+    fig, ax = plt.subplots(figsize=(9.2, 5))
+    ax.invert_yaxis()
+    ax.xaxis.set_visible(False)
+    ax.set_xlim(0, np.sum(data, axis=1).max())
 
+    for i, (colname, color) in enumerate(zip(classes, category_colors)):
+        widths = data[:, i]
+        starts = data_cum[:, i] - widths
+        rects = ax.barh(labels, widths, left=starts, height=0.5,
+                        label=colname, color=color)
 
+        # r, g, b, _ = color
+        # text_color = 'white' if r * g * b < 0.5 else 'darkgrey'
+        ax.bar_label(rects, label_type='center', color= 'black')
+    ax.legend(ncol=len(classes), bbox_to_anchor=(0, -0.1),
+              loc='lower left', fontsize='small')
+
+    return fig, ax
+
+#%% Plot distribution in districts
+
+df_SH_cluster = df_social_housing[df_social_housing['clustered-social-housing'] == True]
+df_SH_No_cluster = df_social_housing[df_social_housing['clustered-social-housing'] == False]
+
+# baseline
+fig, ax = horizontal_barplot(df_baseline)
+ax.set_title('Geographical segregation, baseline')
+ax.set_xlabel('Percentage occupation in each area')
+
+fig.get_figure().savefig(output_fol + "geographical_segregation_bars_baseline.pdf",
+                 bbox_inches='tight')
+
+# clustered social housing
+values = df_social_housing['number-socialhousing'].unique()
+
+for i in values:
+    
+    df_chosen = df_SH_cluster[df_SH_cluster['number-socialhousing'] == i]
+    
+    fig, ax = horizontal_barplot(df_chosen)
+    ax.set_title(f'Geographical segregation with clustered social housing, n = {i}')
+    ax.set_xlabel('Percentage occupation in each area')
+
+    fig.get_figure().savefig(output_fol + f"geographical_segregation_bars_SH_{i}.pdf",
+                     bbox_inches='tight')
+
+# not clustered social housing
+
+for i in values:
+    
+    df_chosen = df_SH_No_cluster[df_SH_No_cluster['number-socialhousing'] == i]
+    
+    fig, ax = horizontal_barplot(df_chosen)
+    ax.set_title(f'Geographical segregation with non-clustered social housing, n = {i}')
+    ax.set_xlabel('Percentage occupation in each area')
+
+    fig.get_figure().savefig(output_fol + f"geographical_segregation_bars_SH_noCluster_{i}.pdf",
+                     bbox_inches='tight')
 
 
 
